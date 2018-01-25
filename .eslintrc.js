2b2a54e5b6cbda3ffd154dbf7094e6eec38cdea2
宋均均 <sjj107294@alibaba-inc.com>
2018-01-25 20:39:58 +0800

    feat: alt

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
 
   rules: {
     // 对象最后一个属性后的逗号
-    'comma-dangle': [2, 'never'],
+    'comma-dangle': 1,
     // 模块加载必须换行
     'import/newline-after-import': 0,
     // 箭头函数用小括号括起来
